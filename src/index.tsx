fd1f5ec00dbf22bb3498a43db2fe65e86be22313
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2023-11-03 22:59:11 +0100

    switch to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { sendToVercelAnalytics } from "./vitals";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
-
-reportWebVitals(sendToVercelAnalytics);
